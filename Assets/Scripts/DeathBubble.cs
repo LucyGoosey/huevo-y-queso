3f0a573e97db4c1d7cf099cbd0a7681b23d452b7
Lucy Fletcher <LucyKFletcher@gmail.com>
2015-01-13 18:25:42 +0000

    First commit

---
+++ Assets/Scripts/DeathBubble.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class DeathBubble : MonoBehaviour {
+
+    public void SetPlayer(int _playerNum)
+    {
+        GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Bubble_" + _playerNum.ToString());
+    }
+}
---
+++ Assets/Trigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D _coll)
+    {
+        if (_coll.tag == "Player")
+            Debug.Log("Player in trigger.");
+    }
+}
