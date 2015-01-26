540892b21f7192d136f70e6fcb5004919538c003
Lucy Fletcher <LucyKFletcher@gmail.com>
2015-01-26 22:18:20 +0000

    updating ,gitignore, like every other commit. Update autozip.pu to have spaces not tabs.

--- Autozip.py
+++ Autozip.py
@@ -5,34 +5,34 @@ import lzma
 metas = []
 
 for root, dirs, files in os.walk("Assets"):
-	for i in range(0, len(files)):
-		if ".meta" in files[i]:
-			if root == "":
-				metas.append(files[i])
-			else:
-				metas.append(root + "/" + files[i])
+    for file in files:
+        if ".meta" in file:
+            if root == "":
+                metas.append(file)
+            else:
+                metas.append(root + "/" + file)
 
 tarFilename = "ZippedUp.tar.xz"
 
 if(os.path.isfile(tarFilename)):
-	print("Old ZippedUp.tar.xz found! Deleting...")
-	os.remove(tarFilename)
+    print("Old ZippedUp.tar.xz found! Deleting...")
+    os.remove(tarFilename)
 
 xzFile = lzma.LZMAFile(tarFilename, mode="w")
 
 with tarfile.open(mode="w", fileobj=xzFile) as tar_xz_file:
-	print("Adding project settings...")
-	tar_xz_file.add("ProjectSettings/")
-	print("Adding scenes...")
-	tar_xz_file.add("Assets/Scenes/")
-	print("Adding prefabs...")
-	tar_xz_file.add("Assets/Prefabs/")
-	print("Adding sprites...")
-	tar_xz_file.add("Assets/Sprites/")
+    print("Adding project settings...")
+    tar_xz_file.add("ProjectSettings/")
+    print("Adding scenes...")
+    tar_xz_file.add("Assets/Scenes/")
+    print("Adding prefabs...")
+    tar_xz_file.add("Assets/Prefabs/")
+    print("Adding sprites...")
+    tar_xz_file.add("Assets/Sprites/")
 	
-	print("Adding metas...")
-	for i in range(0, len(metas)):
-		tar_xz_file.add(metas[i]);
+    print("Adding metas...")
+    for meta in metas:
+        tar_xz_file.add(meta);
 	
 xzFile.close()
 print("All done!")
