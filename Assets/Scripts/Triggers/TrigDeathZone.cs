50ec89ab5b8086a47698fff20249b7aea080948b
Lucy Fletcher <LucyKFletcher@gmail.com>
2015-01-27 23:23:28 +0000

    Adding death zone trigger.

---
+++ Assets/Scripts/Triggers/TrigDeathZone.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class TrigDeathZone : MonoBehaviour 
+{
+    void OnTriggerEnter2D(Collider2D _coll)
+    {
+        if (_coll.tag == "Player")
+            _coll.gameObject.GetComponent<Mario>().OnKill();
+    }
+}
